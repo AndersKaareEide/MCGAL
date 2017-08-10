7d370114b2605f16ae41859b76f0296034352b24
Anders Eide <Anders.kaare.eide@gmail.com>
2017-08-10 17:16:08 +0200

    Setup completely basic structure for representing Models, minus relations for each agent. Created Show instance for Models and their valuation functions Started on a formula checking function to check formulas against states in models

---
+++ src/Base.hs
@@ -0,0 +1,47 @@
+module Base where
+
+data Formula p =
+    Prop p                          |        Neg  (Formula p)
+  | Conj (Formula p) (Formula p)    |        Disj (Formula p) (Formula p)
+-- TODO Expand with complete list of symbols
+
+newtype Agent = Ag Int deriving (Eq, Ord)
+instance Show Agent where
+  show (Ag n) = "a" ++ show n
+
+data Model state prop = Mo {
+      states      :: [state],
+      actors      :: [Agent],
+      props       :: [prop],
+      valuation   :: prop -> [state]
+}
+
+instance (Show state, Show prop) => Show (Model state prop) where
+  show (Mo states actors props valuation) =
+        "{ States: "     ++ show states ++ " "  ++
+        "Actors: "       ++ show actors ++ " "  ++
+        "Propositions:"  ++ show props  ++ " "  ++
+        "Valuations: "   ++ valuations  ++ " }" where
+          valuations = show $ map (\prop -> show prop ++ " -> " ++ (show $ valuation prop)) props
+
+
+-- Each prop is tied to a list of states where the prop holds
+-- TODO Represent the knowledge of each Agent
+-- TODO Fix quotes around each state when the valuation function is printed
+
+check :: (Eq state) => Model state a -> state -> Formula a -> Bool
+check model state (Prop prop) = state `elem` valuation model prop
+
+
+exampleModel :: Model Int String
+exampleModel = Mo {
+    states = [1..3],
+    actors = [Ag x | x <- [1..3]],
+    props = ["p", "q"],
+    valuation = exampleValuation
+  }
+
+exampleValuation :: String -> [Int]
+exampleValuation "p" = [1,2]
+exampleValuation "q" = [1]
+exampleValuation _   = []
