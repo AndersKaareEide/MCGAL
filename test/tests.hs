f27b152b4eede89785b06d392518388d78313024
Anders Eide <Anders.kaare.eide@gmail.com>
2017-08-15 15:47:11 +0200

    Implemented checking of group announcements and set up basic test case for it, but it should probably be more thoroughly tested Also removed unneeded list of propositions from models, as this info is already present in the valuation map, might do the same for actors

--- test/tests.hs
+++ test/tests.hs
@@ -7,7 +7,10 @@ main :: IO Counts
 main = Test.HUnit.runTestTT tests
 
 tests :: Test
-tests = TestList [testKOperators, testAnnouncements, testModelUpdate]
+tests = TestList [
+                  testKOperators, testAnnouncements,
+                  testModelUpdate, testGroupAnnouncement
+                  ]
 
 testKOperators :: Test
 testKOperators = TestCase (assertEqual "Knows-operator is broken" True result) where
@@ -24,3 +27,10 @@ testModelUpdate =
   TestCase (assertEqual "Updates should be true if the announcement is untrue in the present state" True result) where
     result = check exampleModel2 2 formula where
       formula = Announce (Prop "p") (Prop "p")
+
+testGroupAnnouncement :: Test
+testGroupAnnouncement =
+    TestCase (assertEqual "Group announcements are broken" True result) where
+      result = check exampleModel3 1 formula where
+        formula = GroupAnnounce coalition (Knows (Ag 3) (Conj (Prop "p") (Prop "q"))) where
+          coalition = [Ag 1, Ag 2]
