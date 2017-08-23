f96b5b09c950285208919aa31e5bddcc3ceba9c3
Anders Eide <Anders.kaare.eide@gmail.com>
2017-08-23 17:44:01 +0200

    Holy fucking shit, dragable elements! You do not want to know how this is implemented, trust me.

--- src/WebGUI.hs
+++ src/WebGUI.hs
@@ -28,6 +28,12 @@ main = do
 
 setup :: Window -> UI ()
 setup w = do
+        -- If TPG actually let you read the values of attributes this wouldn't
+        -- be neccessary. Thanks for making read ops return () TPG.
+        let startPos = (200,200)
+        let elDragDims = (50,50)
+
+
         -- active elements
         elRemove <- UI.button # set UI.text "Clear"
         elResult <- UI.span
@@ -37,11 +43,13 @@ setup w = do
         elParent <- UI.div #  set style [("height", "100px"), ("width", "100px"),
                                         ("border-style", "solid")]
         elChild <- UI.button # set text "Child"
+        elDragable <- UI.div # set style (getDragableStyle ++ mkPosAttr startPos)
 
         UI.pure elParent #+ [UI.pure elChild]
 
-        eventsRef <- liftIO $ newIORef [] :: UI (IORef [(String, IO ())])
-        inputs   <- liftIO $ newIORef []
+        eventsRef   <- liftIO $ newIORef [] :: UI (IORef [(String, IO ())])
+        dragableRef <- liftIO $ newIORef (False, startPos) :: UI (IORef (Bool,(Int, Int)))
+        inputs      <- liftIO $ newIORef [] :: UI (IORef [Element])
 
 
         -- functionality
@@ -53,7 +61,10 @@ setup w = do
             redoLayout :: UI ()
             redoLayout = void $ do
                 layout <- mkLayout =<< liftIO (readIORef inputs)
-                getBody w # set children  (elCanvas : [layout])
+                let body = getBody w
+                let mainDiv = fromJust <$> getElementById w "mainDiv"
+                mainDiv # set children  (elCanvas : [layout])
+                -- TODO Figure out why div doesn't work
                 displayTotal
 
             mkLayout :: [Element] -> UI Element
@@ -64,6 +75,7 @@ setup w = do
                 [UI.hr
                 ,row [UI.span # set text "Sum: ", element elResult]
                 ,UI.pure elParent
+                ,UI.pure elDragable
                 ]
 
             addInput :: (Int, Int) ->  UI ()
@@ -81,7 +93,9 @@ setup w = do
         on UI.mousedown elCanvas $ \pos -> addInput pos   >> redoLayout
         on UI.click elRemove $ \_ -> removeInput >> redoLayout
 
-        -- Try stopping event propagation
+        -- Try stopping event propagation by storing each event in an IORef
+        -- and only dispatch event from parent if the child element has not
+        -- already fired one
 
         on UI.click elParent $ \event -> do
           events <- liftIO $ readIORef eventsRef
@@ -97,13 +111,51 @@ setup w = do
           void $ liftIO $ modifyIORef eventsRef (("child", print "child") :)
           liftIO $ handleEvents eventsRef
 
-        -- Maybe store event as an ioRef and check ioRefs to see whether to
-        -- dispatch a second event or not
+        -- Register that the user is about to drag
+        on UI.mousedown elDragable $ const $ void $
+          liftIO $ modifyIORef dragableRef (\(_, pos) -> (True, pos))
+
+        on UI.mouseup elDragable $ const $ void $
+          liftIO $ modifyIORef dragableRef (\(_, pos) -> (False, pos))
+
+        on UI.mousemove elDragable $ \mousePos -> do
+          dragging <- liftIO $ fst <$> readIORef dragableRef
+          if dragging
+            then do
+              oldPos <- snd <$> liftIO (readIORef dragableRef)
+              let newPos = calculatePos mousePos oldPos elDragDims
+              void $ liftIO $ modifyIORef dragableRef (\(bool, _) -> (bool, newPos))
+              moveElement elDragable newPos
+            else
+              UI.pure () -- Do nothing
+
 
         redoLayout
-        -- TODO change (50,50)
 
 handleEvents :: IORef [(String, IO ())] -> IO ()
 handleEvents ref = do
   list <- readIORef ref
   snd . head $ list
+
+-- Makes top and left style attributes with the input position values
+mkPosAttr :: (Int, Int) -> [(String, String)]
+mkPosAttr (x,y) = [("left", show x ++ "px"),
+                   ("top", show y ++ "px")]
+
+moveElement :: Element -> (Int, Int) -> UI ()
+moveElement element (x,y) = do
+  let pos = [("left", show x ++ "px"), ("top", show y ++ "px")]
+  void $ UI.pure element # set style (getDragableStyle ++ pos)
+
+
+getDragableStyle :: [(String, String)]
+getDragableStyle = [("height", "50px"), ("width", "50px"),
+                    ("border-style", "solid"),
+                    ("position", "absolute"),
+                    ("background", "blue")]
+
+-- Calculates the new position of an element based on the position of the mouse,
+-- the elements current position and its dimensions
+calculatePos :: (Int, Int) -> (Int, Int) -> (Int, Int) -> (Int, Int)
+calculatePos mousePos oldPos dims =
+  (fst mousePos + (fst oldPos - fst dims `div` 2), snd mousePos + (snd oldPos - snd dims `div` 2))
