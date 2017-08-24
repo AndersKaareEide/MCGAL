844ed1576dd3c95929c9222fb1644eda8ee74a21
Anders Eide <Anders.kaare.eide@gmail.com>
2017-08-24 17:23:51 +0200

    Cleaned out a bunch of unused stuff

--- src/WebGUI.hs
+++ src/WebGUI.hs
@@ -37,7 +37,6 @@ setup w = do
         -- active elements
         elBody   <- UI.getBody w
         elRemove <- UI.button # set UI.text "Clear"
-        elResult <- UI.span
         elCanvas <- UI.div # set style [("height", "500px"), ("width", "100%"),
                                         ("border-style", "solid")]
 
@@ -45,79 +44,74 @@ setup w = do
                                         ("border-style", "solid")]
         elChild <- UI.button # set text "Child"
 
-        UI.pure elParent #+ [UI.pure elChild]
+        element elParent #+ [element elChild]
 
         eventsRef   <- liftIO $ newIORef [] :: UI (IORef [(String, IO ())])
-        dragableRef <- liftIO $ newIORef (Nothing, startPos) :: UI (IORef (Maybe Element,(Int, Int)))
+        draggableRef <- liftIO $ newIORef (Nothing, startPos) :: UI (IORef (Maybe Element,(Int, Int)))
         inputs      <- liftIO $ newIORef [] :: UI (IORef [Element])
 
 
         -- functionality
         let
-            displayTotal = void $ do
-                xs <- mapM (get value) =<< liftIO (readIORef inputs)
-                element elResult
-
             redoLayout :: UI ()
             redoLayout = void $ do
                 layout <- mkLayout =<< liftIO (readIORef inputs)
                 let body = getBody w
                 body # set children  (elCanvas : [layout])
                 -- TODO Figure out why using mainDiv doesn't work
-                displayTotal
 
             mkLayout :: [Element] -> UI Element
             mkLayout xs = column $
-                [row [element elRemove]
-                ,UI.hr]
+                [row [element elRemove]]
                 ++ map element xs ++
-                [UI.hr
-                ,row [UI.span # set text "Sum: ", element elResult]
-                ,UI.pure elParent
-                ]
+                [element elParent]
+
 
             addInput :: (Int, Int) ->  UI ()
             addInput position = do
-                let elInput = UI.input # set value "Dixxxx"
+                let elInput = UI.input # set value "State"
                                        # set style inputStyle
                 elBox <- UI.div # set style boxStyle #+ [elInput]
-                makeDragable elBox
+                makeDraggable elBox
                 liftIO $ modifyIORef inputs (elBox:)
                 where
-                  boxStyle = getDragableStyle ++ mkPosAttr position
+                  boxStyle = getDraggableStyle ++ mkPosAttr position
                   inputStyle = [("width", "80%")]
 
-            removeInput :: UI ()
-            removeInput = liftIO $ writeIORef inputs []
+            clearCanvas :: UI ()
+            clearCanvas = liftIO $ writeIORef inputs []
 
-            makeDragable :: Element -> UI ()
-            makeDragable element = do
-
-              -- Drag functionality
+            -- Adds event listeners to the element to allow it to be
+            -- dragged around on the canvas
+            makeDraggable :: Element -> UI ()
+            makeDraggable element = do
+              -- TODO Eat event and do nothing if a child element was clicked
+              --      rather than the state itself
               on UI.mousedown element $ const $ void $
-                liftIO $ modifyIORef dragableRef (\(_, pos) -> (Just element, pos))
+                liftIO $ modifyIORef draggableRef (\(_, pos) -> (Just element, pos))
 
               on UI.mouseup elBody $ const $ void $
-                liftIO $ modifyIORef dragableRef (\(_, pos) -> (Nothing, pos))
+                liftIO $ modifyIORef draggableRef (\(_, pos) -> (Nothing, pos))
 
               on UI.mousemove elBody $ \mousePos -> do
-                dragging <- liftIO $ fst <$> readIORef dragableRef
+                dragging <- liftIO $ fst <$> readIORef draggableRef
                 case dragging of
                   Just element -> do
-                    oldPos <- snd <$> liftIO (readIORef dragableRef)
-                    void $ liftIO $ modifyIORef dragableRef (\(bool, _) -> (bool, mousePos))
+                    oldPos <- snd <$> liftIO (readIORef draggableRef)
+                    void $ liftIO $ modifyIORef draggableRef (\(bool, _) -> (bool, mousePos))
                     moveElement element mousePos
                   _ ->
                     UI.pure () -- Do nothing
 
         -- TODO Crop elements that fit outside the canvas
         on UI.mousedown elCanvas $ \pos -> addInput pos >> redoLayout
-        on UI.click elRemove $ \_ -> removeInput >> redoLayout
+        on UI.click elRemove $ \_ -> clearCanvas >> redoLayout
 
         -- Try stopping event propagation by storing each event in an IORef
         -- and only dispatch event from parent if the child element has not
         -- already fired one
-
+        -- TODO Make generic system that automagically checks for events from
+        -- child elements before handling event
         on UI.click elParent $ \event -> do
           events <- liftIO $ readIORef eventsRef
           if "child" `notElem` map fst events
@@ -132,10 +126,8 @@ setup w = do
           void $ liftIO $ modifyIORef eventsRef (("child", print "child") :)
           liftIO $ handleEvents eventsRef
 
-
-
-
         redoLayout
+
 
 handleEvents :: IORef [(String, IO ())] -> IO ()
 handleEvents ref = do
@@ -146,11 +138,11 @@ handleEvents ref = do
 moveElement :: Element -> (Int, Int) -> UI ()
 moveElement element (x,y) = do
   let pos = [("left", show x ++ "px"), ("top", show y ++ "px")]
-  void $ UI.pure element # set style (getDragableStyle ++ pos)
+  void $ UI.pure element # set style (getDraggableStyle ++ pos)
 
--- Returns style tuples for dragable elements
-getDragableStyle :: [(String, String)]
-getDragableStyle = [("height", "50px"), ("width", "50px"),
+-- Returns style tuples for Draggable elements
+getDraggableStyle :: [(String, String)]
+getDraggableStyle = [("height", "50px"), ("width", "50px"),
                     ("border-style", "solid"),
                     ("position", "absolute"),
                     ("background", "blue")]
