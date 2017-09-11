d8981058d5eaee255cf342fa11a03c426109ba4b
DESKTOP-L2SCKMK\AndersLaptop <Anders.kaare.eide@gmail.com>
2017-09-11 12:21:02 +0200

    Make constant value an actual constant

--- src/main/kotlin/canvas/CanvasConstants.kt
+++ src/main/kotlin/canvas/CanvasConstants.kt
@@ -4,4 +4,4 @@ package canvas
  * File containing various constants used in layouts related to the Canvas
  */
 
-val STATE_CIRCLE_RADIUS: Double = 25.0
+const val STATE_CIRCLE_RADIUS: Double = 25.0
