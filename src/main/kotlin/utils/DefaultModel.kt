e9dff5d9c8b3bca1f106a2ce10a1bdf61f342451
Anders Eide <Anders.kaare.eide@gmail.com>
2017-09-26 14:21:19 +0200

    Updated the default model

--- src/main/kotlin/utils/DefaultModel.kt
+++ src/main/kotlin/utils/DefaultModel.kt
@@ -2,17 +2,16 @@ package utils
 
 import canvas.data.AgentItem
 import canvas.data.Edge
-import canvas.data.Model
 import canvas.data.State
 import javafx.collections.FXCollections
 import sidepanels.propertypanel.PropositionItem
 
-private val propP = PropositionItem("p", false)
+private val propP = PropositionItem("p", true)
 private val propQ = PropositionItem("q", false)
 
 private val agentA = AgentItem("a", true)
-private val agentB = AgentItem("b", true)
-private val agentC = AgentItem("c", true)
+private val agentB = AgentItem("b", false)
+private val agentC = AgentItem("c", false)
 
 private val state1 = State("s1", 150.0, 200.0, FXCollections.observableArrayList(propP))
 private val state2 = State("s2", 50.0, 70.0, FXCollections.observableArrayList(propP, propQ))
