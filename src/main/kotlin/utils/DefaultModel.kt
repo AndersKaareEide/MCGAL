41d7b739d77816c982e75d1e8081c3e182f09a5b
Anders Eide <Anders.kaare.eide@gmail.com>
2017-10-06 15:33:01 +0200

    Updated default model to comply with the new parsing rules

--- src/main/kotlin/utils/DefaultModel.kt
+++ src/main/kotlin/utils/DefaultModel.kt
@@ -6,21 +6,19 @@ import canvas.data.State
 import javafx.collections.FXCollections
 import sidepanels.propertypanel.PropositionItem
 
-private val propP = PropositionItem("p", true)
-private val propQ = PropositionItem("q", false)
+private val propP = PropositionItem("regn", true)
 
-private val agentA = AgentItem("a", true)
-private val agentB = AgentItem("b", false)
-private val agentC = AgentItem("c", false)
+private val agentA = AgentItem("Arne", true)
+private val agentB = AgentItem("Bjarne", false)
 
 private val state1 = State("s1", 150.0, 200.0, FXCollections.observableArrayList(propP))
-private val state2 = State("s2", 50.0, 70.0, FXCollections.observableArrayList(propP, propQ))
+private val state2 = State("s2", 50.0, 70.0, FXCollections.observableArrayList())
 
 private val edgeS1S2 = Edge(state1, state2, mutableListOf(agentA))
 
 val defaultStates = listOf(state1, state2)
 val defaultEdges = listOf(edgeS1S2)
-val defaultAgents = listOf(agentA, agentB, agentC)
-val defaultProps = listOf(propP, propQ)
+val defaultAgents = listOf(agentA, agentB)
+val defaultProps = listOf(propP)
 
 
