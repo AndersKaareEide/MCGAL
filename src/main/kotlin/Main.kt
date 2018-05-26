a1b869f8ab0d04bcb41b2f904c771f40e46b6dbb
Anders Eide <Anders.kaare.eide@gmail.com>
2018-05-26 16:42:39 +0200

    Added actual main function as the TornadoFX plugin keeps 'forgetting' which class is the main class of the application

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import canvas.styles.ModelStyles
 import canvas.views.Canvas
 import formulafield.styling.LabelStyling
+import javafx.application.Application
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyCodeCombination
 import menus.styles.MenuBarStyles
@@ -12,4 +13,8 @@ class Main : App(Canvas::class, *styles){
     init {
         FX.layoutDebuggerShortcut = KeyCodeCombination(KeyCode.F1)
     }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(Main::class.java, *args)
 }
